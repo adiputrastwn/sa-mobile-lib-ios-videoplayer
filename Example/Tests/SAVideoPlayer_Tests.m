8e63d3131799540e7efd30066cade74f2f2a33ec
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-10-27 13:06:01 +0100

    1.1.8 - tried to (and probably) fixed the audio bug when the phisical mute button is off

--- Example/Tests/SAVideoPlayer_Tests.m
+++ Example/Tests/SAVideoPlayer_Tests.m
@@ -27,80 +27,80 @@
 
 - (void) testVideo1 {
     
-//    // when
-//    NSString *path = [[NSBundle mainBundle] pathForResource:@"preroll" ofType:@"mp4"];
-//    __block BOOL reachedStart = false;
-//    __block BOOL reached14 = false;
-//    __block BOOL reached12 = false;
-//    __block BOOL reached34 = false;
-//    __block BOOL reachedEnd = false;
-//    __block BOOL reachedError = false;
-//    __block int stage = 0;
-//    
-//    // then
-//    XCTAssertNotNil(_vc);
-//    XCTAssertNotNil(_vc.view);
-//    
-//    SAVideoPlayer *video = [[SAVideoPlayer alloc] initWithFrame:CGRectMake(0, 0, 320, 240)];
-//    
-//    XCTAssertNotNil(video);
-//    
-//    [_vc.view addSubview:video];
-//    
-//    __block XCTestExpectation *expectation = [self expectationWithDescription:@"High Expectations"];
-//    
-//    [video setEventHandler:^(SAVideoPlayerEvent event) {
-//        switch (event) {
-//            case Video_Start: {
-//                reachedStart = true;
-//                stage++;
-//                break;
-//            }
-//            case Video_1_4: {
-//                reached14 = true;
-//                stage++;
-//                break;
-//            }
-//            case Video_1_2: {
-//                reached12 = true;
-//                stage++;
-//                break;
-//            }
-//            case Video_3_4: {
-//                reached34 = true;
-//                stage++;
-//                break;
-//            }
-//            case Video_End: {
-//                reachedEnd = true;
-//                stage++;
-//                [expectation fulfill];
-//                break;
-//            }
-//            case Video_Error: {
-//                reachedError = true;
-//                [expectation fulfill];
-//                break;
-//            }
-//        }
-//    }];
-//    
-//    // finally play!
-//    [video playWithMediaFile:path];
-//    
-//    [self waitForExpectationsWithTimeout:300.0 handler:^(NSError *error) {
-//        if (error) {
-//            NSLog(@"Timeout Error: %@", error);
-//        } else {
-//            XCTAssertTrue(reachedStart);
-//            XCTAssertTrue(reached14);
-//            XCTAssertTrue(reached12);
-//            XCTAssertTrue(reached34);
-//            XCTAssertTrue(reachedEnd);
-//            XCTAssertFalse(reachedError);
-//            XCTAssertEqual(stage, 5);
-//        }
-//    }];
+    // when
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"preroll" ofType:@"mp4"];
+    __block BOOL reachedStart = false;
+    __block BOOL reached14 = false;
+    __block BOOL reached12 = false;
+    __block BOOL reached34 = false;
+    __block BOOL reachedEnd = false;
+    __block BOOL reachedError = false;
+    __block int stage = 0;
+    
+    // then
+    XCTAssertNotNil(_vc);
+    XCTAssertNotNil(_vc.view);
+    
+    SAVideoPlayer *video = [[SAVideoPlayer alloc] initWithFrame:CGRectMake(0, 0, 320, 240)];
+    
+    XCTAssertNotNil(video);
+    
+    [_vc.view addSubview:video];
+    
+    __block XCTestExpectation *expectation = [self expectationWithDescription:@"High Expectations"];
+    
+    [video setEventHandler:^(SAVideoPlayerEvent event) {
+        switch (event) {
+            case Video_Start: {
+                reachedStart = true;
+                stage++;
+                break;
+            }
+            case Video_1_4: {
+                reached14 = true;
+                stage++;
+                break;
+            }
+            case Video_1_2: {
+                reached12 = true;
+                stage++;
+                break;
+            }
+            case Video_3_4: {
+                reached34 = true;
+                stage++;
+                break;
+            }
+            case Video_End: {
+                reachedEnd = true;
+                stage++;
+                [expectation fulfill];
+                break;
+            }
+            case Video_Error: {
+                reachedError = true;
+                [expectation fulfill];
+                break;
+            }
+        }
+    }];
+    
+    // finally play!
+    [video playWithMediaFile:path];
+    
+    [self waitForExpectationsWithTimeout:300.0 handler:^(NSError *error) {
+        if (error) {
+            NSLog(@"Timeout Error: %@", error);
+        } else {
+            XCTAssertTrue(reachedStart);
+            XCTAssertTrue(reached14);
+            XCTAssertTrue(reached12);
+            XCTAssertTrue(reached34);
+            XCTAssertTrue(reachedEnd);
+            XCTAssertFalse(reachedError);
+            XCTAssertEqual(stage, 5);
+        }
+    }];
 }
 
 - (void) testVideo2 {
